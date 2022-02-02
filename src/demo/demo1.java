f569673a9c9ce9069b6112698bbdd6d544077ab9
adarsh vishwakarma <adarsh.cool.31@gmail.com>
2022-02-02 05:34:28 +0530

    Test

---
+++ src/demo/demo1.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class demo1 {
+	
+	public static void test()
+	
+	{
+		System.out.println("This is demo method");
+	}
+
+	
+	public static void main(String[] args)
+	{
+		demo1 obj = new demo1();
+		obj.test();
+	}
+}
